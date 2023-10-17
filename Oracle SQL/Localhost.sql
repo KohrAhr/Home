d50ea37d04f2e19567caabd7da030073cd18d7fd
ZAM <ZAM@1CLICK.LV>
2023-10-17 20:01:28 +0300

    Oracle SQL script

---
+++ Oracle SQL/Localhost.sql
@@ -0,0 +1,6 @@
+create user "TestUser" IDENTIFIED BY "Password"
+
+//-- alter USER ANONYMOUS IDENTIFIED BY "second_2nd_pwd"
+
+
+//-- alter USER ANONYMOUS IDENTIFIED BY @db.password("second_2nd_pwd")
